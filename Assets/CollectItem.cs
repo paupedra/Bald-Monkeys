915d6e1da1a38548cb5d476fc9bcb4d69c3c84b1
M1R4B3L <amirabelmartinez@gmail.com>
2021-02-04 12:55:18 +0100

    Just to detect collisions

---
+++ Assets/CollectItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectItem : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);    
+    }
+
+}
