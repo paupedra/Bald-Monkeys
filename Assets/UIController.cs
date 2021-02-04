4eee64fa63d8055c0c2276042cf9ffecf625ea35
paupedra <paupe200@gmail.com>
2021-02-04 20:40:15 +0100

    Added some UI to GameScene

---
+++ Assets/UIController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //pauseMenu = GameObject.Find("Pause Menu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("escape"))
+        {
+            if(pauseMenu.activeSelf)
+            {
+                pauseMenu.SetActive(false);
+            }
+            else
+            {
+                pauseMenu.SetActive(true);
+            }
+        }
+
+
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
